8c84fb939fe162c79287f090a04c4fb8cc243c02
sujanlam <106173760+sujanlam@users.noreply.github.com>
2024-05-25 12:46:40 -0500

    Update FindSecondlargestNumber.java

--- src/java8streams/FindSecondlargestNumber.java
+++ src/java8streams/FindSecondlargestNumber.java
@@ -19,6 +19,7 @@ public class FindSecondlargestNumber {
         List<Integer> sortedNumsDesc = numbers.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
         sortedNumsDesc.forEach(System.out::print);
         System.out.println();
+        //Second Largest Number in a list
         int secondlargest = numbers.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().get();
         System.out.println(secondlargest);
     }
