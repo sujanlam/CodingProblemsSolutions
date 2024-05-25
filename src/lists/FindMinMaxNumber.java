a6b053d05ff386679bdee4122888413d9ebfb9cb
Sujan Lam <sujann42@gmail.com>
2024-05-25 08:24:15 -0500

    added more comments

--- src/lists/FindMinMaxNumber.java
+++ src/lists/FindMinMaxNumber.java
@@ -3,7 +3,8 @@ package lists;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-//Using Java 8 Streams
+
+//Using Java 8 Streams to find min and max
 public class FindMinMaxNumber {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(10,12,5,7,19,0,1,211);
